1bd05ff9c857b60dc65b02d9b6a66806d9fb7618
Cal Courbois <ccourbois@unc.edu>
2022-03-01 10:18:24 -0500

    ex06

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,46 @@
+"""Dictionaries or smthn? Idk, I cant read."""
+
+
+__author__ = "730482131"
+
+
+def invert(switch: dict[str, str]) -> dict:
+    """Switch a roo."""
+    swap: dict[str, str]
+    swap = dict()
+    for x, y in switch.items():
+        swap[y] = x
+    return swap
+
+
+def favorite_color(color: dict[str, str]) -> str:
+    """Chooses most common color."""
+    fav: str = ""
+    tally: dict[str, int]
+    tally = dict()
+    for key in color:
+        if color[key] in tally:
+            tally[color[key]] += 1
+        else:
+            tally[color[key]] = 1
+    amount: int = 0
+    for key in tally:
+        if amount < tally[key]:
+            amount = tally[key]
+            fav = key
+    return fav
+
+
+def count(things: list[str]) -> dict[str, int]:
+    """Kinda what I did in the last fxn but easier."""
+    digit: dict[str, int]
+    digit = dict()
+    i: int = 0
+    while i < len(things):
+        if things[i] in digit:
+            digit[things[i]] += 1
+            i += 1
+        else:
+            digit[things[i]] = 1
+            i += 1
+    return digit
---
+++ exercises/ex06/dictionary_test.py
@@ -0,0 +1,66 @@
+"""We gotta test it to make sure I'm not just cheating lol. But idk how I'd cheat in a dictionary..."""
+
+
+__author__ = "730482131"
+
+
+from dictionary import invert
+from dictionary import favorite_color
+from dictionary import count
+
+
+# Invert tests
+def test_invert_empty() -> None:
+    """Doo a switch-a-roo but empty."""
+    switch = {}
+    assert invert(switch) == {}
+
+
+def test_invert_one_set() -> None:
+    """Doo a switch-a-roo withy one pair."""
+    switch = {"a": "z"}
+    assert invert(switch) == {"z": "a"}
+
+
+def test_invert_two_sets() -> None:
+    """Doo a switch-a-roo with multiple pairs."""
+    switch = {"a": "z", "b": "y"}
+    assert invert(switch) == {"z": "a", "y": "b"}
+
+
+# Fav colors!!
+def test_favorite_color_empty() -> None:
+    """I dont like this function so I refuse to give it a description."""
+    color = {"": ""}
+    assert favorite_color(color) == ""
+
+
+def test_favorite_color_three() -> None:
+    """Dont think because I have to do it twice that I'll cave."""
+    color = {"cal": "blue", "jackie": "pink", "hugh": "blue"}
+    assert favorite_color(color) == "blue"
+
+
+def test_favorite_color_tie() -> None:
+    """I swear, it took me forever :/ ."""
+    color = {"cal": "blue", "jackie": "pink", "hugh": "blue", "kiera": "pink"}
+    assert favorite_color(color) == "blue"
+
+
+# count
+def test_count_empty() -> None:
+    """This was easier, it makes a dictionary with the amount of times a word is in a list."""
+    things = []
+    assert count(things) == {}
+
+
+def test_count_double() -> None:
+    """This was easier, it makes a dictionary with the amount of times a word is in a list."""
+    things = ["cal", "jackie", "jackie"]
+    assert count(things) == {"cal": 1, "jackie": 2}
+
+
+def test_count_lots() -> None:
+    """This was easier, it makes a dictionary with the amount of times a word is in a list."""
+    things = ["cal", "jackie", "jackie", "cal", "jackie", "jackie", "cal", "jackie", "jackie"]
+    assert count(things) == {"cal": 3, "jackie": 6}
